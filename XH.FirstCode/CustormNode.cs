43d10e53cc7648d66e853e93de0e93abe21e5f96
苑海龙 <y2328109996@163.com>
2024-05-22 19:25:08 +0800

    FirstCode

---
+++ XH.FirstCode/CustormNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace XH.FirstCode
+{
+    public class CustormNode : UIElement
+    {
+
+    }
+}
