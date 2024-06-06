d7d1c992d3511afc61e9ae679f3b612b3dc0e509
苑海龙 <y2328109996@163.com>
2024-06-06 22:31:21 +0800

    20240606_WPF对象变形

---
+++ XH.WPFObjectDeformation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace XH.WPFObjectDeformation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
