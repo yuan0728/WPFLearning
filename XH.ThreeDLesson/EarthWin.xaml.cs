060310ead9ddc74015fb24b2a1a7d341500970bc
苑海龙 <y2328109996@163.com>
2024-06-23 22:50:05 +0800

    8_Over

---
+++ XH.ThreeDLesson/EarthWin.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace XH.ThreeDLesson
+{
+    /// <summary>
+    /// EarthWin.xaml 的交互逻辑
+    /// </summary>
+    public partial class EarthWin : Window
+    {
+        public EarthWin()
+        {
+            InitializeComponent();
+            this.Loaded += EarthWin_Loaded;
+        }
+
+        private void EarthWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.gm.Geometry = this.SetEarth(180, 180, 50);
+        }
+        private MeshGeometry3D SetEarth(int numx, int numz, double r = 3)
+        {
+            MeshGeometry3D mesh = new MeshGeometry3D();
+
+            double dTh = 2 * Math.PI / numx;
+            double dPhi = Math.PI / numz;
+
+            double X(double th, double phi) => r * Math.Sin(phi) * Math.Cos(th);
+            double Y(double th, double phi) => r * Math.Sin(phi) * Math.Sin(th);
+            double Z(double phi) => r * Math.Cos(phi);
+
+            // Make the points.
+            for (int i = 0; i <= numx; i++)
+            {
+                for (int j = 0; j <= numz; j++)
+                {
+                    var th = i * dTh;
+                    var phi = j * dPhi;
+                    mesh.Positions.Add(new Point3D(X(th, phi), Y(th, phi), Z(phi)));
+                    mesh.TextureCoordinates.Add(new Point(th, phi));
+                }
+            }
+
+            // 生成三角形
+            for (int i = 0; i < numx; i++)
+            {
+                for (int j = 0; j < numz; j++)
+                {
+                    int i1 = i * (numz + 1) + j;
+                    int i2 = i1 + 1;
+                    int i3 = i2 + (numz + 1);
+                    int i4 = i3 - 1;
+
+                    mesh.TriangleIndices.Add(i1);
+                    mesh.TriangleIndices.Add(i2);
+                    mesh.TriangleIndices.Add(i3);
+
+                    mesh.TriangleIndices.Add(i1);
+                    mesh.TriangleIndices.Add(i3);
+                    mesh.TriangleIndices.Add(i4);
+                }
+            }
+            return mesh;
+        }
+    }
+}
---
+++ XH.ThreeDLesson/HelixToolkitWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace XH.ThreeDLesson
+{
+    /// <summary>
+    /// HelixToolkitWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class HelixToolkitWindow : Window
+    {
+        public HelixToolkitWindow()
+        {
+            InitializeComponent();
+            this.Loaded += HelixToolkitWindow_Loaded;
+        }
+
+        private void HelixToolkitWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] model_files = System.IO.Directory.GetFiles($"{Environment.CurrentDirectory}/3D_Models");
+
+            ModelImporter importer = new ModelImporter();
+
+            Model3DGroup group = new Model3DGroup();
+            foreach (string file in model_files)
+            {
+                // 模型文件解析 过程 ==> GeometryModel3D
+                var mg = importer.Load(file);
+
+                group.Children.Add(mg.Children[0]);
+            }
+            this.model.Content = group;
+        }
+    }
+}
