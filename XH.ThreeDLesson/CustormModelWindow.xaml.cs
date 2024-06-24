8860ebf85c4d9ae0b1998e40839167b9e46a75b5
苑海龙 <y2328109996@163.com>
2024-06-24 20:53:33 +0800

    9_Over

---
+++ XH.ThreeDLesson/CustormModelWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XH.ThreeDLesson
+{
+    /// <summary>
+    /// CustormModelWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class CustormModelWindow : Window
+    {
+        public CustormModelWindow()
+        {
+            InitializeComponent();
+            this.Loaded += CustormModelWindow_Loaded;
+        }
+
+        private void CustormModelWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ModelImporter importer = new ModelImporter();
+
+
+            model.Content = importer.Load("D:\\Users\\yhl18\\Desktop\\WPF学习\\WPFLearning\\XH.ThreeDLesson\\bin\\Debug\\net7.0-windows\\3D_Models\\无标题.obj");
+        }
+    }
+}
