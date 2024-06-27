9aadadec9eb63e2dc12964d4c939690db4e9f343
苑海龙 <y2328109996@163.com>
2024-06-27 22:34:54 +0800

    13_Over

---
+++ XH.LayoutLesson/XHPanel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace XH.LayoutLesson
+{
+    public class XHPanel : Panel
+    {
+        // 测量
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            // availableSize 整个区域
+            // 遍历所有的子项
+            foreach (UIElement item in this.InternalChildren)
+            {
+                item.Measure(availableSize);
+            }
+            return availableSize;
+        }
+        // 排列
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            foreach(UIElement item in this.InternalChildren)
+            {
+                item.Arrange(new Rect(0, 0, finalSize.Width, finalSize.Height));
+            }
+            return base.ArrangeOverride(finalSize);
+        }
+    }
+}
