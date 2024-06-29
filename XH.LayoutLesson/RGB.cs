bb6472af54a2d117c7ddf9d57cf249c1eea8af64
苑海龙 <y2328109996@163.com>
2024-06-29 21:54:00 +0800

    14_over

---
+++ XH.LayoutLesson/RGB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace XH.LayoutLesson
+{
+    public class RGB : MarkupExtension
+    {
+        public byte Alpha { get; set; } = 255; // 颜色深浅度
+        byte R, G, B;
+        public RGB(byte r, byte g, byte b)
+        {
+            R = r; G = g; B = b;
+        }
+        public RGB(byte a, byte r, byte g, byte b)
+        {
+            Alpha = a; R = r; G = g; B = b;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            // 0 0 0 黑色
+            // 255 255 255 白色
+            return new SolidColorBrush(Color.FromArgb(Alpha,R, G, B));
+        }
+    }
+}
