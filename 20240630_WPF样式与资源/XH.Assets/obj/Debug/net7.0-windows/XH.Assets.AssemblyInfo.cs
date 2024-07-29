6df01e10e4d790a07fd233c294c3a969f4fa7a57
苑海龙 <y2328109996@163.com>
2024-07-29 23:47:58 +0800

    1、MVVM分层架构下的弹窗逻辑 2、窗口管理对象的封装 3、弹窗逻辑中的数据传递 4、逻辑管理对象封装 5、逻辑管理对象中的数据传递

--- 20240630_WPF样式与资源/XH.Assets/obj/Debug/net7.0-windows/XH.Assets.AssemblyInfo.cs
+++ 20240630_WPF样式与资源/XH.Assets/obj/Debug/net7.0-windows/XH.Assets.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XH.Assets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42e8daa68dacc368af7ccb57a0033b8deccb7ded")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc5897e1b1ef6a7629761da188bd9ad17880c103")]
 [assembly: System.Reflection.AssemblyProductAttribute("XH.Assets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XH.Assets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
