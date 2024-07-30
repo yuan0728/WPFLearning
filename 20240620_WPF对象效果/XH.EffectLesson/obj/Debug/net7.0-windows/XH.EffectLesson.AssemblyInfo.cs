ee1a10bc20e6c144714eb0cc92f1d8ecbff08a07
苑海龙 <y2328109996@163.com>
2024-07-30 23:17:03 +0800

    1、MvvmLight框架准备 2、MvvmLight中的相关基类 3、MvvmLight中的命令对象 4、Messenger对象基本使用 5、Messenger双向委托调用 6、DispatcherHelper对象使用

--- 20240620_WPF对象效果/XH.EffectLesson/obj/Debug/net7.0-windows/XH.EffectLesson.AssemblyInfo.cs
+++ 20240620_WPF对象效果/XH.EffectLesson/obj/Debug/net7.0-windows/XH.EffectLesson.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XH.EffectLesson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039fc4b1a8b000360e495c24cf46e1aa07511e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b13647ec3a45f524ef008cc00dd4568014414be")]
 [assembly: System.Reflection.AssemblyProductAttribute("XH.EffectLesson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XH.EffectLesson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
